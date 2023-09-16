fd73bd14f5dc02da81ace24dc91b2bb90be8011b
Mohammad Khaled Safi <51378060+MohammedSafi96@users.noreply.github.com>
2023-09-16 16:49:32 +0300

    Add files via upload

---
+++ ProtoTypeDesignPattren/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtoTypeDesignPattren
+{
+    internal partial class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int DepartmentID { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format(" Name : {0}  " + "DepartmentID : {1}  {2} ",
+                this.Name, this.DepartmentID.ToString(), AddressDetails?.ToString());
+        }
+    }
+   // internal partial class Employee : ICloneable // Shallow Copy 
+    internal partial class Employee : CloneablePrototype<Employee> // Deep Copy 
+    {
+        public object Clone()
+        {
+            // Returns a new object instance that is a memberwise copy of this
+            // object.  This is always a shallow copy of the instance. The method is protected
+            // so that other object may only call this method on themselves.  It is intended to
+            // support the ICloneable interface. 
+            return this.MemberwiseClone();
+        }
+    }
+    internal partial class Employee
+    {
+        public Address AddressDetails { get; set; }
+    }
+
+    internal class Address
+    {
+        public Address() { }
+
+        public int DoorNumber { get; set; }
+        public int StreetNumber { get; set; }
+        public int Zipcode { get; set; }
+        public string Country { get; set; }
+        public override string ToString()
+        {
+            return string.Format("AddressDetails : Door : {0}, Street: {1}, ZipCode : {2}," +
+                " Country : {3}", this.DoorNumber, this.StreetNumber, this.Zipcode.ToString(),
+                this.Country);
+        }
+    }
+}
---
+++ ProtoTypeDesignPattren/Program.cs
@@ -0,0 +1,101 @@
+﻿using ProtoTypeDesignPattren;
+//ShallowCopy();
+//ShallowCopyRef();
+DeepCopy();
+
+// ShallowCopy
+static void ShallowCopy()
+{
+    Employee empM = new Employee()
+    {
+        Id = Guid.NewGuid(),
+        Name = "Mohammad",
+        DepartmentID = 150,
+    };
+
+    Employee empGh = (Employee)empM.Clone();
+    empGh.Name = "Ghazal";
+
+    Console.WriteLine(empM.ToString());
+    Console.WriteLine(empGh.ToString());
+
+    Console.WriteLine("////////////////////////////");
+    Console.WriteLine("Changed Mohammad DepartmentID to 161");
+    empM.DepartmentID = 161;
+    Console.WriteLine(empM.ToString());
+    Console.WriteLine(empGh.ToString());
+
+    Console.ReadLine();
+}
+static void ShallowCopyRef()
+{
+    Employee empM = new Employee()
+    {
+        Id = Guid.NewGuid(),
+        Name = "Mohammad",
+        DepartmentID = 150,
+        AddressDetails = new Address()
+        {
+            DoorNumber = 10,
+            StreetNumber = 20,
+            Zipcode = 90025,
+            Country = "JO"
+        }
+    };
+
+    Console.WriteLine(empM.ToString());
+
+    Employee empGh = (Employee)empM.Clone();
+    empGh.Name = "Ghazal";
+    empGh.DepartmentID = 151;
+    empGh.AddressDetails.StreetNumber = 21;
+    empGh.AddressDetails.DoorNumber = 11;
+
+    Console.WriteLine(empGh.ToString());
+
+    Console.WriteLine("////////////////////////////");
+    Console.WriteLine("Modified Details of Mohammad");
+    empM.AddressDetails.DoorNumber = 30;
+    empM.AddressDetails.StreetNumber = 40;
+
+    empM.DepartmentID = 160;
+    Console.WriteLine(empM.ToString());
+    Console.WriteLine(empGh.ToString());
+
+    Console.ReadLine();
+}
+
+static void DeepCopy()
+{
+    Employee empM = new Employee()
+    {
+        Id = Guid.NewGuid(),
+        Name = "Mohammad",
+        DepartmentID = 150,
+        AddressDetails = new Address()
+        {
+            DoorNumber = 10,
+            StreetNumber = 20,
+            Zipcode = 90025,
+            Country = "JO"
+        }
+    };
+
+    Console.WriteLine(empM.ToString());
+
+    Employee empGh = (Employee)empM.DeepCopy();
+
+    empGh.Name = "Ghazal";
+    empGh.DepartmentID = 151;
+
+    Console.WriteLine(empGh.ToString());
+
+    Console.WriteLine("Modified Details of Mohammad");
+    empM.AddressDetails.DoorNumber = 30;
+    empM.AddressDetails.StreetNumber = 40;
+
+    empM.DepartmentID = 160;
+    Console.WriteLine(empM.ToString());
+    Console.WriteLine(empGh.ToString());
+    Console.ReadLine();
+}
